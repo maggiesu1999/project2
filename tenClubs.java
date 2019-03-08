791d203d74d32d7ef52766dea49129fd63ae26f3
maggiesu1999 <48339079+maggiesu1999@users.noreply.github.com>
2019-03-07 21:05:34 -0500

    Update tenClubs.java

--- tenClubs.java
+++ tenClubs.java
@@ -2,8 +2,17 @@
 public class tenClubs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GolfClub one = new GolfClub(230,30);
+		GolfClub two = new GolfClub(215,20);
+		GolfClub three = new GolfClub(180,20);
+		GolfClub four = new GolfClub(170,17);
+		GolfClub five = new GolfClub(155,15);
+		GolfClub six = new GolfClub(145,15);
+		GolfClub seven = new GolfClub(135,15);
+		GolfClub eight = new GolfClub(125,15);
+		GolfClub nine = new GolfClub(110,10);
+		GolfClub ten = new GolfClub(50,10);
+		
 	}
 
 }

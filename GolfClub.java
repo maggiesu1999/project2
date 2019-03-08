49da6d23a3898b1735be94a55b6b22288fc0298d
maggiesu1999 <48339079+maggiesu1999@users.noreply.github.com>
2019-03-07 20:36:35 -0500

    Update GolfClub.java

--- GolfClub.java
+++ GolfClub.java
@@ -1,5 +1,13 @@
-package GolfClub;
-
 public class GolfClub {
+       protected int mean;
+       protected int sd;
+       public GolfClub1(int mean, int sd){
+              this.mean = mean;
+              this.sd = sd;
+       }
+         // setter and getters;
+       
 
 }
+
+

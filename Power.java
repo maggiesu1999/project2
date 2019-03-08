07051099c04ac139b906d376130fcd0ea1b09cc5
maggiesu1999 <48339079+maggiesu1999@users.noreply.github.com>
2019-03-07 21:22:12 -0500

    Update Power.java

--- Power.java
+++ Power.java
@@ -6,4 +6,19 @@ public class Power {
           this.mean= mean;
           this.sd = sd;
         }
+        public void setMean(int mean){
+              this.mean = mean;
+       }
+       
+       public int getMean(){
+              return mean;
+       }
+
+       public void setSd(int sd){
+              this.sd = sd;
+       }
+       
+       public int getSd(){
+              return sd;
+       }
 }

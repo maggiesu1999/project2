1ef76be550ae787814d7a212e9172475c7cb7957
qfu4 <47185724+qfu4@users.noreply.github.com>
2019-03-07 20:42:33 -0500

    add getter/setter

--- GolfClub.java
+++ GolfClub.java
@@ -1,13 +1,29 @@
 public class GolfClub {
+       protected String 
        protected int mean;
        protected int sd;
        public GolfClub1(int mean, int sd){
               this.mean = mean;
               this.sd = sd;
        }
-         // setter and getters;
+       // setter and getters;
+       public void setMean(int mean){
+              this.mean = mean;
+       }
        
+       public int getMean(){
+              return mean;
+       }
 
+       public void setSd(int sd){
+              this.sd = sd;
+       }
+       
+       public int getSd(){
+              return sd;
+       }
+       
+       
 }
 
 

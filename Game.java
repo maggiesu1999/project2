d248ad19dd12322c8bce86fb9079249b2a0c9335
qfu4 <47185724+qfu4@users.noreply.github.com>
2019-03-07 21:39:37 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -12,7 +12,6 @@ public class Game {
 		System.out.println("Please select a course:" + "\n" + "1. Genesee Valley Park North Course" + 
 				   "2. The Old Course at St.Andrews");
 		userChoice = scan.nextInt();
-		System.out.println("Your choice [1-2]: " + userChoice);
 		if (userChoice == 1){
 			System.out.println("You are playing at Genesee Valley Park North Course");
 		}

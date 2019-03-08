562e00698dc3372d2b122456fbcf0974738312ca
maggiesu1999 <48339079+maggiesu1999@users.noreply.github.com>
2019-03-07 21:29:42 -0500

    Update tenPowers.java

--- tenPowers.java
+++ tenPowers.java
@@ -1,4 +1,16 @@
 
 public class tenPowers {
-
+  public static void main(String[] args) {
+    Power pow10 = new Power(40,5);
+    Power pow9 = new Power(30,5);
+    Power pow8 = new Power(25,4);
+    Power pow7 = new Power(20,4);
+    Power pow6 = new Power(16,3);
+    Power pow5 = new Power(12,3);
+    Power pow4 = new Power(8,2);
+    Power pow3 = new Power(4,2);
+    Power pow2 = new Power(2,1);
+    Power pow1 = new Power(1,1);
+       
+  }
 }

3995db9ee1ab3dbf3152287351f5f7bf35adad9b
qfu4 <47185724+qfu4@users.noreply.github.com>
2019-03-07 20:52:35 -0500

    add distance

--- GolfClub.java
+++ GolfClub.java
@@ -1,8 +1,9 @@
+import java.util.Random;
 public class GolfClub {
-       protected String 
-       protected int mean;
-       protected int sd;
-       public GolfClub1(int mean, int sd){
+       private String name;
+       private int mean;
+       private int sd; 
+       public GolfClub1(String name, int mean, int sd){
               this.mean = mean;
               this.sd = sd;
        }
@@ -22,6 +23,15 @@ public class GolfClub {
        public int getSd(){
               return sd;
        }
+       
+       //
+       public int nextDistance(int power){
+              double meanAdj = mean * power / 10.0;
+              double sdAdj = sd * power / 10.0;
+              int userDistance = (int)(Math.abs(random.nextGaussian() * sdAdj + meanAdj));
+              return userDistance;
+       }
+       
        
        
 }

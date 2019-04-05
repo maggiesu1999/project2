e62ab6a9d6250d26205a2caedc851f3cf7e1a773
qfu4 <47185724+qfu4@users.noreply.github.com>
2019-04-04 21:22:40 -0400

    Update firework.java

--- firework.java
+++ firework.java
@@ -1,1 +1,16 @@
-import 
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+
+
+  
+
+public class Firework extends JComponent implements ActionListener{
+  
+  
+  
+  
+  
+  
+  
+  
+}
